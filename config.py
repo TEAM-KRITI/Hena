5759470a4e8e765cf8ab3e2936c30a3d2bf0c220
AHarshit14363 <harshitayush181@gmail.com>
2025-05-01 10:20:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badnam019/STRANGER-MUSIC",
+    "https://github.com/AHarshit14363/Maahi_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
