274ea53a61ccf652771491eb1fb89d64cfd36251
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-05-24 19:41:05 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from SHUKLAMUSIC.utils.database import is_on_off
+from SHUKLAMUSIC.utils.formatters import time_to_seconds
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
         cmd,
