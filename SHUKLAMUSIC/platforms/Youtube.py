ef9b8aecc24b66591e438f7b2118b586cf39c734
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-06-19 01:05:03 +0530

    Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from SHUKLAMUSIC.utils.database import is_on_off
+from SHUKLAMUSIC.utils.formatters import time_to_seconds
 
 
 def cookie_txt_file():
